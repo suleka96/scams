e4146b16d6ac4331c83fc9b70ce5a33f283712d8
sajithaliyanage <sajithaliyanage@gmail.com>
2019-01-26 21:47:24 +0530

    Deploy to production account

---
+++ src/components/Page404/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css'
+
+const Page404 = () => (
+  	<div id="notfound">
+		<div className="notfound">
+			<div className="notfound-404">
+				<h1>4<span>0</span>4</h1>
+			</div>
+			<h2>the page you requested could not found</h2>
+		</div>
+	</div>
+);
+
+export default Page404;
